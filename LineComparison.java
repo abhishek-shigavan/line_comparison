4651d35fd0475fa4f0aa6b64b8dfb154b649929e
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2021-06-09 22:09:30 +0530

    Add: Checking if the lines are equal

--- LineComparison.java
+++ LineComparison.java
@@ -1,18 +1,48 @@
 public class LineComparison {
 
-	public static void main(String[] args) {
+	static int lengthOfLine(int x1,int x2,int y1,int y2) {
 
-		//point A coordinates
-		int x1 = 2;
-		int y1 = 3;
-		//point B coordinates
-		int x2 = 9;
-		int y2 = 6;
+		int length=(int)(Math.sqrt( ((x2-x1)*(x2-x1)) + ((y2-y1)*(y2-y1)) ));
+		return length;
+	}
+
+	public static void main(String[] args) {
 
 		System.out.println("Welcome to Line Comparison Program...!!");
 
-		int lengthOfLine=(int)(Math.sqrt( ((x2-x1)*(x2-x1)) + ((y2-y1)*(y2-y1)) ));
+		//point A coordinates
+		int xa;
+		int ya;
+		//point B coordinates
+		int xb;
+		int yb;
 
-		System.out.println("Length of the line : " + lengthOfLine);
+		//line1
+		xa=2;
+		ya=3;
+		xb=9;
+		yb=6;
+
+		String lengthOfLine1=Integer.toString(lengthOfLine(xa,xb,ya,yb));
+
+		System.out.println("Length of line 1 : "+ lengthOfLine1);
+
+		//line 2
+		xa=1;
+		ya=3;
+		xb=2;
+		yb=9;
+
+		String lengthOfLine2=Integer.toString(lengthOfLine(xa,xb,ya,yb));
+
+		System.out.println("Length of line 2 : "+ lengthOfLine2);
+
+		if (lengthOfLine1.equals(lengthOfLine2))
+		{
+			System.out.println("Line 1 and Line 2 are of equal length");
+		}
+		else
+			System.out.println("Line 1 and Line 2 are not of equal length");
 	}
+
 }
