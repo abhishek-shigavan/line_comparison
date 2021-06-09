8420c886e66e92624fe8352377d6c24286b3e6c6
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2021-06-09 21:04:46 +0530

    Add: Length Of Line Computed

--- LineComparison.java
+++ LineComparison.java
@@ -2,7 +2,17 @@ public class LineComparison {
 
 	public static void main(String[] args) {
 
+		//point A coordinates
+		int x1 = 2;
+		int y1 = 3;
+		//point B coordinates
+		int x2 = 9;
+		int y2 = 6;
+
 		System.out.println("Welcome to Line Comparison Program...!!");
 
+		int lengthOfLine=(int)(Math.sqrt( ((x2-x1)*(x2-x1)) + ((y2-y1)*(y2-y1)) ));
+
+		System.out.println("Length of the line : " + lengthOfLine);
 	}
 }
