b96481a4f03fb912a218e6587497e03e192a7d86
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2021-06-10 13:44:34 +0530

    Refactor: Checking the length of line equal or not

--- LineComparison.java
+++ LineComparison.java
@@ -1,48 +1,87 @@
+/**
+ * LineComparison --- program to check length of two lines
+ *                    are equal or not
+ *
+ * @author    Abhishek Shigavan
+ */
+import java.util.*;
+
 public class LineComparison {
 
-	static int lengthOfLine(int x1,int x2,int y1,int y2) {
+	//variables for line coordinates
+	static int xa, xb, ya, yb;
 
-		int length=(int)(Math.sqrt( ((x2-x1)*(x2-x1)) + ((y2-y1)*(y2-y1)) ));
-		return length;
+/**
+   * Takes point coordinates from user
+   * and store it in array
+	   * @return return array.
+  */
+	static int[] getUserInput() {
+
+		Scanner sc = new Scanner(System.in);
+		int[] userInput = new int[4];
+
+		System.out.println("Enter x coordinates for point A");
+		xa= sc.nextInt();
+		System.out.println("Enter y coordinates for point A");
+		ya= sc.nextInt();
+		System.out.println("Enter x coordinates for point B");
+		xb= sc.nextInt();
+		System.out.println("Enter y coordinates for point B");
+		yb= sc.nextInt();
+
+		userInput[0]=xa;
+		userInput[1]=ya;
+		userInput[2]=xb;
+		userInput[3]=yb;
+
+		return userInput;
 	}
+
+/**
+ 	* Calculate line length
+	*
+	* @return return length of line.
+  */
+	static int lengthOfLine(int x1,int x2,int y1,int y2) {
+			int length=(int)(Math.sqrt( ((x2-x1)*(x2-x1)) + ((y2-y1)*(y2-y1)) ));
+			return length;
+		}
 
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to Line Comparison Program...!!");
 
-		//point A coordinates
-		int xa;
-		int ya;
-		//point B coordinates
-		int xb;
-		int yb;
+		System.out.println("Enter Coordinates for Line 1");
+		int coordinatOfLine1[]=getUserInput();
 
-		//line1
-		xa=2;
-		ya=3;
-		xb=9;
-		yb=6;
+		xa=coordinatOfLine1[0];
+		xb=coordinatOfLine1[2];
+		ya=coordinatOfLine1[1];
+		yb=coordinatOfLine1[3];
+
 
 		String lengthOfLine1=Integer.toString(lengthOfLine(xa,xb,ya,yb));
-
 		System.out.println("Length of line 1 : "+ lengthOfLine1);
 
-		//line 2
-		xa=1;
-		ya=3;
-		xb=2;
-		yb=9;
+		System.out.println("Enter Coordinates for Line 2");
+		int coordinateOfLine2[]=getUserInput();
+
+		xa=coordinateOfLine2[0];
+		xb=coordinateOfLine2[2];
+		ya=coordinateOfLine2[1];
+		yb=coordinateOfLine2[3];
 
 		String lengthOfLine2=Integer.toString(lengthOfLine(xa,xb,ya,yb));
-
 		System.out.println("Length of line 2 : "+ lengthOfLine2);
 
-		if (lengthOfLine1.equals(lengthOfLine2))
-		{
-			System.out.println("Line 1 and Line 2 are of equal length");
+		if (lengthOfLine1.equals(lengthOfLine2)){
+			System.out.println("Line 1 and Line 2 are of Equal Length");
 		}
-		else
-			System.out.println("Line 1 and Line 2 are not of equal length");
+		else{
+			System.out.println("Line 1 and Line 2 are not of equal length" );
+		}
 	}
 
 }
+
